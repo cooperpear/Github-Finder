609f16dd6edcb61b96734a4cb9c3247f68599a95
Cooper Pearson <mptprogesterone@gmail.com>
2020-07-08 13:21:36 -0500

    finishes User interface User experience

--- ui.js
+++ ui.js
@@ -12,10 +12,10 @@ class UI {
                         <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
                     </div>
                     <div class="col-md-9"> 
-                        <span class="badge bade-primary">Public Repos: ${user.public_repos}</span>
-                        <span class="badge bade-secondary">Public Gists: ${user.public_gists}</span>
-                        <span class="badge bade-success">Followers: ${user.followers}</span>
-                        <span class="badge bade-info">Following: ${user.following}</span>
+                        <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
+                        <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
+                        <span class="badge badge-success">Followers: ${user.followers}</span>
+                        <span class="badge badge-info">Following: ${user.following}</span>
                     <br>
                     <br>
                     <ul class="list-group">
@@ -30,6 +30,31 @@ class UI {
             <h3 class="page-heading mb-3">Latest Repos</h3>
             <div id="repos"> </div>
         `;
+    }
+
+    //Show user Repos
+    showRepos(repos) {
+        let output = '';
+
+        repos.forEach(function (repo) {
+            output += `
+                <div class="card card-body mb-2">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                        </div>
+                        <div class="col-md-6">
+                            <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+                            <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
+                            <span class="badge badge-success">Forks: ${repo.forms_count}</span>
+                        </div>
+                    </div>
+                </div>
+            `
+        });
+
+        //Output the repositories
+        document.getElementById('repos').innerHTML = output;
     }
 
     //show alert message

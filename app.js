753ef8129673c0f78def144cb1b624c4101f66c2
Cooper Pearson <mptprogesterone@gmail.com>
2020-07-08 11:32:04 -0500

    public keys for github access. fetch data from github data and store. return data to user

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const searchUser = document.getElementById('searchUser');
 searchUser.addEventListener('keyup', (e) => {
     //Get input text
     const userText = e.target.value;
-
+    //if userText is not empty, Then 
     if (userText !== '') {
         console.log(userText);
     }
